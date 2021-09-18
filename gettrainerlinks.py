0514c0685a8005c7f3f69c071ac16aace60db58e
nickfff-dev <59580486+nickfff-dev@users.noreply.github.com>
2021-09-18 18:19:10 +0300

    Update gettrainerlinks.py

--- gettrainerlinks.py
+++ gettrainerlinks.py
@@ -1,3 +1,4 @@
+#start here to get trainer files from each manufacterer url
 import re
 from bs4 import BeautifulSoup
 import requests
@@ -12,6 +13,7 @@ driver = webdriver.Chrome()
 
 pages = ["https://tech-labs.com/manufacturers/bayport-technical"]
 
+# use this snippet for manufacterers with pagination
 # ukurasa = ["https://tech-labs.com/manufacturers/amatrol?page=0%2C"]
 
 # tejas = ["0","1","2","3","4","5","6","7","8"]
